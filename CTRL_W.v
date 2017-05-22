8a48fccdd62d14b3f88fd1215337f9612185adf1
fkd151136 <15061188@buaa.edu.cn>
2017-05-22 23:08:12 +0800

    Update CTRL_W.v

--- CTRL_W.v
+++ CTRL_W.v
@@ -19,13 +19,13 @@ W_DEC DEC_CTRL_W(IR[31:21],IR_FUNC,lb,lbu,lh,lhu,lw,add,addu,sub,subu,sll,srl,sr
 						_and,_or,_xor,_nor,addi,addiu,andi,ori,xori,lui,slt,slti,sltiu,sltu,jal,jalr,mfc0);
 	
 	assign Wreg_sel=(jal)?2'd2:
-						 (add|addu|sub|subu|slt|sltu|jalr|sll|srl|sra|sllv|srlv|srav|_and|_or|_xor|_nor)?2'd1:2'd0;
+					(add|addu|sub|subu|slt|sltu|jalr|sll|srl|sra|sllv|srlv|srav|_and|_or|_xor|_nor)?2'd1:2'd0;
 	assign Wdata_sel=(jal|jalr)?2'd2:
-						  (lb|lbu|lh|lhu|lw|mfc0)?2'd1:2'd0;
+					 (lb|lbu|lh|lhu|lw|mfc0)?2'd1:2'd0;
 	assign GRF_WE=add|addu|sub|subu|ori|lui|lw|jal|slt|slti|sltiu|sltu|jalr|lb|lbu|lh|lhu|sll|srl|sra|sllv|srlv|srav
-					 |_and|_or|_xor|_nor|addi|addiu|andi|ori|xori|lui|mfc0;
+				 |_and|_or|_xor|_nor|addi|addiu|andi|ori|xori|lui|mfc0;
 	assign XEXT_OP=(lh) ?3'd4:
-						(lhu)?3'd3:
-						(lb) ?3'd2:
-						(lbu)?3'd1:3'd0;
+					(lhu)?3'd3:
+					(lb) ?3'd2:
+					(lbu)?3'd1:3'd0;
 endmodule
